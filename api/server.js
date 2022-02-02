b68ed009de79ea53e69b8fb473cce9d0622877fc
sooof <sooof@me.com>
2022-02-01 23:04:18 -0800

    two way get /api/dogs

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 // IMPORTS AT THE TOP
 const express = require('express') // commonjs
+const Dog = require('./dog-model.js')
 // INSTANCE OF EXPRESS APP
 const server = express()
 // GLOBAL MIDDLEWARE
@@ -13,6 +14,26 @@ server.get('/hello_world', (req, res) => {
     res.status(200).json('hello world!!!!!!')
   })
 // [GET]    /api/dogs     (R of CRUD, fetch all dogs)
+// server.get('/api/dogs', async(req, res) => {
+//     // res.json('all the dogs')
+//     try{
+//         const dogs = await Dog.findAll()
+//         res.json(dogs)
+//     }catch(err) {
+//         res.status(500).json({message: err.message})
+//     }
+// })
+
+server.get('/api/dogs', (req, res) => {
+    // res.json('all the dogs')
+    Dog.findAll()
+        .then( dogs =>{
+            res.json(dogs)
+        })
+        .catch( err => {
+            res.status(500).json({message: err.message})
+        })
+})
 // [GET]    /api/dogs/:id (R of CRUD, fetch dog by :id)
 // [POST]   /api/dogs     (C of CRUD, create new dog from JSON payload)
 // [PUT]    /api/dogs/:id (U of CRUD, update dog with :id using JSON payload)
