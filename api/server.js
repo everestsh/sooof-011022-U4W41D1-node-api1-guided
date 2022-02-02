8445bd35da2ee770dacffc74f7f5c8c5acd5817f
sooof <sooof@me.com>
2022-02-01 23:05:37 -0800

    test  get /api/dogs/1

--- api/server.js
+++ api/server.js
@@ -35,6 +35,20 @@ server.get('/api/dogs', (req, res) => {
         })
 })
 // [GET]    /api/dogs/:id (R of CRUD, fetch dog by :id)
+server.get('/api/dogs/:id', async (req, res) => {
+    console.log(req.method)
+    console.log(req.headers)
+    console.log(req.body)
+    console.log(req.params)
+    res.json('test req by endpoint')
+    // try {
+    //   const dog = await Dog.findById()
+    // //   res.json(dog)
+    // } catch (err) {
+    //   res.status(500).json({ message: err.message })
+    // }
+  })
+  
 // [POST]   /api/dogs     (C of CRUD, create new dog from JSON payload)
 // [PUT]    /api/dogs/:id (U of CRUD, update dog with :id using JSON payload)
 // [DELETE] /api/dogs/:id (D of CRUD, remove dog with :id)
