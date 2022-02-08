533d1a3d48d9660d0f22cf8ed4fdf9281f0af27a
sooof <sooof@me.com>
2022-02-08 00:07:52 -0800

    express run

--- index.js
+++ index.js
@@ -1,21 +1,45 @@
 // import the server and start it!
-const http = require("http"); // built in node.js module to handle http traffic
+// const http = require("http"); // built in node.js module to handle http traffic
+const express = require('express');
 
-const hostname = "127.0.0.1"; // the local computer where the server is running
 const port = 3000; // a port we'll use to watch for traffic
 
-const server = http.createServer((req, res) => {
-    // creates our server
-    res.statusCode = 200; // http status code returned to the client
-    res.setHeader("Content-Type", "text/plain"); // inform the client that we'll be returning text
-    res.end("Hello World from Node\n"); // end the request and send a response with the specified message
-});
+const server = express();
 
-server.listen(port, hostname, () => {
+// const server = http.createServer((req, res) => {
+//     // creates our server
+//     res.statusCode = 200; // http status code returned to the client
+//     res.setHeader("Content-Type", "text/plain"); // inform the client that we'll be returning text
+//     res.end("Hello World from Node\n"); // end the request and send a response with the specified message
+// });
+
+server.get('/', (req, res)=> {
+  res.send('Hello world from Express!')
+})
+
+server.listen(port, () => {
     // start watching for connections on the port specified
-    console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running on port ${port}/`);
 });
 
 //Run CMD
-//node index.js 
-//
+//  node index.js 
+//  npm init -y
+//  yarn add express
+//  npx eslint --init
+//  npx gitignore node
+//  npm i -D nodemon 
+//  npm install express nanoid
+
+// curl localhost:9000/hello_world -v
+
+
+
+/** 
+ * 12> del git cached
+```
+git rm -r --cached .
+git add .
+git commit -m 'update .gitignore'
+```
+*/
